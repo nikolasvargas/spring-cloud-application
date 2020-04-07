504a71eac3a466c61265d1a2525239ac171c69ee
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-07 15:17:35 -0300

    purchase data object

---
+++ store/src/main/java/br/com/programming/store/controller/dto/ProviderInfoDTO.java
@@ -0,0 +1,13 @@
+package br.com.programming.store.controller.dto;
+
+public class ProviderInfoDTO {
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
