da61beb0d9a63c846ed4ba4a193777e834c20b08
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-07 15:17:54 -0300

    fix whitespace and added purchase service

--- store/src/main/java/br/com/programming/store/controller/PurchaseController.java
+++ store/src/main/java/br/com/programming/store/controller/PurchaseController.java
@@ -1,19 +1,24 @@
 package br.com.programming.store.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.programming.store.controller.dto.PurchaseDTO;
+import br.com.programming.store.service.PurchaseService;
 
 @RestController
 @RequestMapping("/purchase")
 public class PurchaseController {
 
+	@Autowired
+	private PurchaseService purchaseService;
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public void purchase(@RequestBody PurchaseDTO purchase) {
-
+		purchaseService.purchase(purchase);
 	}
 
 }
