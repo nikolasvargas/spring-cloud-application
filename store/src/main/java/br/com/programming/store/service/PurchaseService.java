4cb2b1292eb9c31c8490a7f859cc77eb1e54e169
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-08 01:47:55 -0300

    @Service decorator missed

--- store/src/main/java/br/com/programming/store/service/PurchaseService.java
+++ store/src/main/java/br/com/programming/store/service/PurchaseService.java
@@ -2,13 +2,15 @@ package br.com.programming.store.service;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.programming.store.controller.dto.ProviderInfoDTO;
 import br.com.programming.store.controller.dto.PurchaseDTO;
 
+@Service
 public class PurchaseService {
-	
+
 	public void purchase(PurchaseDTO purchase) {
 		RestTemplate client = new RestTemplate();
 		ResponseEntity<ProviderInfoDTO> exchange = client.exchange(
