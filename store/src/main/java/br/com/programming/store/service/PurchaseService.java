67a01bfea204cfe3d2154da956af6af2c89c25bd
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-09 00:38:34 -0300

    setup RestTemplate to use eureka loadBalance

--- store/src/main/java/br/com/programming/store/service/PurchaseService.java
+++ store/src/main/java/br/com/programming/store/service/PurchaseService.java
@@ -1,5 +1,6 @@
 package br.com.programming.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,12 @@ import br.com.programming.store.controller.dto.PurchaseDTO;
 @Service
 public class PurchaseService {
 
+    @Autowired
+    private RestTemplate client;
+
     public void purchase(PurchaseDTO purchase) {
-        RestTemplate client = new RestTemplate();
         ResponseEntity<ProviderInfoDTO> exchange = client.exchange(
-                "http://provider/info/" + purchase.getAddress().getState(),
-                HttpMethod.GET, null,
+                "http://provider/info/" + purchase.getAddress().getState(), HttpMethod.GET, null,
                 ProviderInfoDTO.class);
         System.out.println(exchange.getBody().getAddress());
     }
