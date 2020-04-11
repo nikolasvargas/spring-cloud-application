3672b8e7a9e7b26e9b159b5000857b945231c928
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-11 17:25:56 -0300

    imporve controller

--- provider/src/main/java/br/com/programming/provider/controller/InfoController.java
+++ provider/src/main/java/br/com/programming/provider/controller/InfoController.java
@@ -1,8 +1,10 @@
 package br.com.programming.provider.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +19,18 @@ public class InfoController {
     @Autowired
     private InfoService infoService;
 
+    @GetMapping()
+    public List<ProviderInfo> getAll() {
+        return infoService.getAll();
+    }
+
     @RequestMapping("/{state}")
     public Optional<ProviderInfo> getInfoByState(@PathVariable String state) {
         return Optional.ofNullable(
                 infoService
                     .getInfoByState(state)
                     .stream()
-                    .findFirst()
+                    .findAny()
                     .get()
                 );
     }
