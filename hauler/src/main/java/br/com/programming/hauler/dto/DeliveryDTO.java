90eb45174637a59890173167b2bbf1d76e80897e
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-19 09:07:33 -0300

    develiry dto class

---
+++ hauler/src/main/java/br/com/programming/hauler/dto/DeliveryDTO.java
@@ -0,0 +1,46 @@
+package br.com.programming.hauler.dto;
+
+import java.time.LocalDate;
+
+public class DeliveryDTO {
+
+    private Long orderId;
+
+    private LocalDate deliveryDate;
+
+    private String providerAddress;
+
+    private String finalAddress;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getProviderAddress() {
+        return providerAddress;
+    }
+
+    public void setProviderAddress(String providerAddress) {
+        this.providerAddress = providerAddress;
+    }
+
+    public String getFinalAddress() {
+        return finalAddress;
+    }
+
+    public void setFinalAddress(String finalAddress) {
+        this.finalAddress = finalAddress;
+    }
+}
