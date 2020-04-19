7f1ef070ae7dfa5cc11a0fbeba8bb4de8e096525
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-19 08:48:57 -0300

    hauler service

---
+++ hauler/src/main/java/br/com/programming/hauler/HaulerApplication.java
@@ -0,0 +1,13 @@
+package br.com.programming.hauler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaulerApplication.class, args);
+	}
+
+}
---
+++ hauler/src/test/java/br/com/programming/hauler/HaulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.programming.hauler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
