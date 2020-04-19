c657ffdb399e1d96efe46a61b74eaeb3f5b9d60f
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-19 15:26:11 -0300

    delivery integration service

--- store/src/main/java/br/com/programming/store/service/PurchaseService.java
+++ store/src/main/java/br/com/programming/store/service/PurchaseService.java
@@ -1,13 +1,19 @@
 package br.com.programming.store.service;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import br.com.programming.store.client.HaulerClient;
 import br.com.programming.store.client.ProviderClient;
+import br.com.programming.store.dto.DeliveryInfoDTO;
 import br.com.programming.store.dto.OrderInfoDTO;
+import br.com.programming.store.dto.ProviderInfoDTO;
 import br.com.programming.store.dto.PurchaseDTO;
+import br.com.programming.store.dto.VoucherInfoDTO;
 import br.com.programming.store.model.Purchase;
 import br.com.programming.store.repository.PurchaseRepository;
 
@@ -16,6 +22,9 @@ public class PurchaseService {
 
     @Autowired
     private ProviderClient providerClient;
+
+    @Autowired
+    private HaulerClient haulerClient;
 
     @Autowired
     private PurchaseRepository purchaseRepository;
@@ -27,9 +36,29 @@ public class PurchaseService {
 
     @HystrixCommand(fallbackMethod = "purchaseFallback", threadPoolKey = "purchaseThreadPool")
     public Purchase purchase(PurchaseDTO purchase) {
+
+        ProviderInfoDTO stateInfo = providerClient.getInfoByState(purchase.getAddress().getState());
+
         OrderInfoDTO order = providerClient.purchaseItem(purchase.getItems());
-        Purchase p = new Purchase(order.getId(), order.getEstimatedTime(), purchase.getAddress().toString());
+
+        DeliveryInfoDTO deliveryInfo = new DeliveryInfoDTO();
+        deliveryInfo.setOrderId(order.getId());
+        deliveryInfo.setDeliveryDate(LocalDate.now().plusDays(order.getEstimatedTime()));
+        deliveryInfo.setProviderAddress(stateInfo.getAddress());
+        deliveryInfo.setFinalAddress(purchase.getAddress().toString());
+
+        VoucherInfoDTO voucherInfo = haulerClient.scheduleDelivery(deliveryInfo);
+
+        Purchase p = new Purchase(
+                order.getId(),
+                voucherInfo.getNumber(),
+                order.getEstimatedTime(),
+                purchase.getAddress().toString(),
+                deliveryInfo.getProviderAddress(),
+                deliveryInfo.getFinalAddress());
+
         purchaseRepository.save(p);
+
         return p;
     }
 
