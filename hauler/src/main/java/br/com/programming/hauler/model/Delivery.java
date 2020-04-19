400fbf6eb23039f7844e201132fba24981f310e2
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-19 11:30:38 -0300

    add entity

---
+++ hauler/src/main/java/br/com/programming/hauler/model/Delivery.java
@@ -0,0 +1,75 @@
+package br.com.programming.hauler.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;
+
+    private LocalDate collectionDate;
+
+    private LocalDate expectedDelivery;
+
+    private String providerAddress;
+
+    private String finalAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getCollectionDate() {
+        return collectionDate;
+    }
+
+    public void setCollectionDate(LocalDate collectionDate) {
+        this.collectionDate = collectionDate;
+    }
+
+    public LocalDate getExpectedDelivery() {
+        return expectedDelivery;
+    }
+
+    public void setExpectedDelivery(LocalDate expectedDelivery) {
+        this.expectedDelivery = expectedDelivery;
+    }
+
+    public String getProviderAddress() {
+        return providerAddress;
+    }
+
+    public void setProviderAddress(String providerAddress) {
+        this.providerAddress = providerAddress;
+    }
+
+    public String getFinalAddress() {
+        return finalAddress;
+    }
+
+    public void setFinalAddress(String finalAddress) {
+        this.finalAddress = finalAddress;
+    }
+
+}
